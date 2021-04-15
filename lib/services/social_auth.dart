da1a5127f11cf96eac4ffc5936951f9388c8f9cb
HaniMohamed <hani.hussein94@gmail.com>
2021-04-15 08:00:10 +0200

    build fix facebook login && add logout and get email functions

--- lib/services/social_auth.dart
+++ lib/services/social_auth.dart
@@ -1,20 +1,19 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
-
-class SocialAuth{
+class SocialAuth {
   Future<UserCredential> signInWithFacebook() async {
     // Trigger the sign-in flow
-    final AccessToken result = (await FacebookAuth.instance.login()) as AccessToken;
-
+    final LoginResult result = await FacebookAuth.instance.login();
     // Create a credential from the access token
     final FacebookAuthCredential facebookAuthCredential =
-    FacebookAuthProvider.credential(result.token);
+        FacebookAuthProvider.credential(result.accessToken.token);
 
     // Once signed in, return the UserCredential
-    return await FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
+    return await FirebaseAuth.instance
+        .signInWithCredential(facebookAuthCredential);
   }
 
   Future<UserCredential> signInWithGoogle() async {
@@ -22,7 +21,8 @@ class SocialAuth{
     final GoogleSignInAccount googleUser = await GoogleSignIn().signIn();
 
     // Obtain the auth details from the request
-    final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
 
     // Create a new credential
     final GoogleAuthCredential credential = GoogleAuthProvider.credential(
@@ -32,5 +32,14 @@ class SocialAuth{
 
     // Once signed in, return the UserCredential
     return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+
+  String getEmail() {
+    return FirebaseAuth.instance.currentUser.email;
+  }
+
+  logout(context) async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.of(context).popAndPushNamed('/login');
   }
 }
