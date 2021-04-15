1783f0e2e37a75e8b2ebe8df4d0afea252b48ff3
HaniMohamed <hani.hussein94@gmail.com>
2021-04-15 08:03:57 +0200

    finish web browser screen with browser-provider

--- lib/ui/webbrowser_screen.dart
+++ lib/ui/webbrowser_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:easacc_task/provider/web_browser_provider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:webview_flutter/webview_flutter.dart';
@@ -10,6 +13,9 @@ class WebBrowserScreen extends StatefulWidget {
 
 class _WebBrowserScreenState extends State<WebBrowserScreen> {
   double lineProgress = 0.0;
+  final Completer<WebViewController> webviewController =
+      Completer<WebViewController>();
+  WebViewController webController;
 
   @override
   void initState() {
@@ -21,8 +27,13 @@ class _WebBrowserScreenState extends State<WebBrowserScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Eassac Browser"),
+        bottomOpacity: .5,
+        title: Text("Easacc Browser"),
         actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.refresh),
+              tooltip: "Refresh web page",
+              onPressed: () => refreshWeb()),
           IconButton(
             icon: Icon(Icons.settings),
             tooltip: 'Open settings',
@@ -32,13 +43,49 @@ class _WebBrowserScreenState extends State<WebBrowserScreen> {
           ),
         ],
       ),
-      body: Consumer<WebBrowserProvider>(
-        builder: (context, browser, child) {
-          return WebView(
-            initialUrl: browser.url,
-          );
-        },
+      body: Stack(
+        children: [
+          webView(),
+          Positioned(
+              child: LinearProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+            value: lineProgress,
+          )),
+        ],
       ),
     );
+  }
+
+  Consumer<WebBrowserProvider> webView() {
+    return Consumer<WebBrowserProvider>(
+      builder: (context, browser, child) {
+        return WebView(
+          initialUrl: browser.url,
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController webViewController) {
+            webviewController.complete(webViewController);
+            browser.registerWebview(webViewController);
+            webController = webViewController;
+          },
+          onProgress: (int progress) {
+            setState(() {
+              lineProgress = progress / 100;
+            });
+            print("WebView is loading (progress : $progress%)");
+          },
+          onPageStarted: (String url) {
+            print('Page started loading: $url');
+          },
+          onPageFinished: (String url) {
+            print('Page finished loading: $url');
+          },
+          gestureNavigationEnabled: true,
+        );
+      },
+    );
+  }
+
+  refreshWeb() {
+    webController.reload();
   }
 }
