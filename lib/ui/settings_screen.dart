81208a1be94940aa2f03030732dbebba00e0f7f2
HaniMohamed <hani.hussein94@gmail.com>
2021-04-15 12:32:18 +0200

    get wifi and bluetooth devices

--- lib/ui/settings_screen.dart
+++ lib/ui/settings_screen.dart
@@ -1,5 +1,6 @@
 import 'package:easacc_task/provider/web_browser_provider.dart';
 import 'package:easacc_task/services/social_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 import 'package:preferences/preference_page.dart';
@@ -21,8 +22,9 @@ class _SettingsScreenState extends State<SettingsScreen> {
   FlutterBlue flutterBlue = FlutterBlue.instance;
 
   List<String> _bluDevices = [];
-
+  String _choosenBlu;
   List<String> _wifiDevices = [];
+  String _choosenWifi;
 
   @override
   void initState() {
@@ -66,17 +68,86 @@ class _SettingsScreenState extends State<SettingsScreen> {
               children: [
                 Row(
                   children: [
+                    IconButton(
+                        icon: Icon(
+                          Icons.bluetooth_searching,
+                          color: Colors.blue,
+                        ),
+                        onPressed: () {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                              content:
+                                  Text("Scanning for bluetooth devices ..")));
+                          _startBluScan();
+                        }),
                     Text("Bluetooth devices: "),
+                    SizedBox(
+                      width: 10,
+                    ),
                     DropdownButton(
+                        hint: Text("select device"),
+                        value: _choosenBlu,
                         items:
                             new List.generate(_bluDevices.length, (int index) {
                           return new DropdownMenuItem(
+                              value: _bluDevices[index],
                               child: new Container(
-                            child: new Text("${_bluDevices[index]}"),
-                            width: 200.0, //200.0 to 100.0
-                          ));
+                                child: new Text(
+                                  "${_bluDevices[index]}",
+                                  style: TextStyle(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ));
                         }),
-                        onChanged: null),
+                        onChanged: (dynamic val) {
+                          setState(() {
+                            _choosenBlu = val;
+                          });
+                        }),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    IconButton(
+                        icon: Icon(
+                          Icons.wifi_tethering,
+                          color: Colors.blue,
+                        ),
+                        onPressed: () {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                              content: Text("Scanning for wifi devices ..")));
+                          _startWifiScan();
+                        }),
+                    Text("Wifi devices: "),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    DropdownButton(
+                        hint: Text("select device"),
+                        value: _choosenWifi,
+                        items:
+                            new List.generate(_wifiDevices.length, (int index) {
+                          return new DropdownMenuItem(
+                              value: _wifiDevices[index],
+                              child: new Container(
+                                child: new Text(
+                                  "[Device #$index], ${_wifiDevices[index]}",
+                                  style: TextStyle(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ));
+                        }),
+                        onChanged: (val) {
+                          setState(() {
+                            print("#############     " + val.toString());
+                            _choosenWifi = val;
+                          });
+                        }),
+                    Expanded(child: Container()),
                   ],
                 )
               ],
@@ -141,15 +212,21 @@ class _SettingsScreenState extends State<SettingsScreen> {
   }
 
   _startBluScan() {
+    int index = 0;
     setState(() {
       _bluDevices = [];
+      _choosenBlu = null;
     });
     flutterBlue.scanResults.listen((results) {
+      _bluDevices = [];
+
       for (ScanResult r in results) {
         setState(() {
-          _bluDevices.add(r.device.name + " => " + r.rssi.toString());
-          print('${r.device.name} found! rssi: ${r.rssi}');
+          print(r.device.name + r.device.type.toString());
+          _bluDevices.add(
+              r.device.name.isNotEmpty ? r.device.name : "[Device #$index]");
         });
+        index++;
       }
     });
     flutterBlue.startScan(timeout: Duration(seconds: 4));
@@ -161,6 +238,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
     final int port = 80;
     setState(() {
       _wifiDevices = [];
+      _choosenWifi = null;
     });
 
     final stream = NetworkAnalyzer.discover(subnet, port);
@@ -168,7 +246,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
       if (addr.exists) {
         setState(() {
           _wifiDevices.add(addr.ip);
-          print('Found device: ${addr.ip}');
+          // print('Found device: ${addr.ip}');
         });
       }
     });
