de64305d6dd1a7f43047b6767043bbdb74b190b5
HaniMohamed <hani.hussein94@gmail.com>
2021-04-15 08:00:42 +0200

    finish web browser provider

--- lib/provider/web_browser_provider.dart
+++ lib/provider/web_browser_provider.dart
@@ -1,13 +1,19 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-
-class WebBrowserProvider extends ChangeNotifier{
+class WebBrowserProvider extends ChangeNotifier {
   String url = "https://easacc.com/";
+  WebViewController webViewController;
 
-  void setUrl(String url){
-    this.url =url;
+  void setUrl(String urlString) {
+    url = urlString;
     notifyListeners();
+  }
+
+  void registerWebview(WebViewController wvc) {
+    webViewController = wvc;
   }
 }
